27a245daa99b83de697ac1237af8389b5dde91e3
mandy0207 <mandyatwork24@gmail.com>
2023-10-09 23:37:56 -0400

    Parallel execution, code refactoring , pom execution

--- src/test/java/com/obsqura/tests/BaseTest.java
+++ src/test/java/com/obsqura/tests/BaseTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import com.obsqura.pages.ExpenseCategoryPage;
@@ -22,48 +23,58 @@ import com.obsqura.utilities.TestProperties;
 public class BaseTest {
 	WebDriver driver;
 	Properties prop;
-	@Parameters({"browserName"})
-	@BeforeClass(alwaysRun=true)
-	public void InitializeDriver() throws IOException {
-        prop = TestProperties.GetProperties();
-        String browserName =  prop.getProperty("browserName");
-        String Environment = prop.getProperty("Environment");
-        String URL=prop.getProperty(Environment);
+
+	@Parameters({ "browserName" })
+	@BeforeClass(alwaysRun = true)
+	public void InitializeDriver(@Optional String browserName) throws IOException {
+		prop = TestProperties.GetProperties();
 		
-		if (browserName.equals("Chrome")) {
-		  driver = new ChromeDriver();
-
-		} else if (browserName.equals("Firefox")) {
-			driver = new FirefoxDriver();
+		if(browserName==null || browserName.isEmpty()) {
+		browserName=prop.getProperty("browserName");
 		}
+		
+		String Environment = prop.getProperty("Environment");
+		String URL = prop.getProperty(Environment);
 
-		else if (browserName.equals("Edge")) {
-			driver = new EdgeDriver();
-		} else {
-			System.out.println("Choose Right Browser");
-		}
+		getDriver(browserName);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get(URL);
 		InitializePages();
-	
+
 	}
-	
+
+	public WebDriver getDriver(String browserName) {
+		switch (browserName.toUpperCase()) {
+		case "CHROME":
+			driver = new ChromeDriver();
+			break;
+		case "FIREFOX":
+			driver = new FirefoxDriver();
+			break;
+		case "EDGE":
+			driver = new EdgeDriver();
+		default:
+			System.out.println("Choose Right Browser");
+		}
+		return driver;
+	}
+
 	public LoginPage lp;
-	public 	HomePage hp;
+	public HomePage hp;
 	public ExpenseCategoryPage ecp;
-	public  ManagePaymentMethodsPage up;
+	public ManagePaymentMethodsPage up;
 	public ListProductPage lpp;
-	
+
 	public void InitializePages() {
-		 lp = new LoginPage(driver);
-		 hp = new HomePage(driver);
-		 ecp = new ExpenseCategoryPage(driver);
-		 up= new  ManagePaymentMethodsPage(driver);
-		 lpp = new ListProductPage(driver);
+		lp = new LoginPage(driver);
+		hp = new HomePage(driver);
+		ecp = new ExpenseCategoryPage(driver);
+		up = new ManagePaymentMethodsPage(driver);
+		lpp = new ListProductPage(driver);
 	}
-	
-	@AfterClass(alwaysRun=true)
+
+	@AfterClass(alwaysRun = true)
 	public void TearDown() {
 		driver.quit();
 	}
